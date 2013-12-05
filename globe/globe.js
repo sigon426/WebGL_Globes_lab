a071ad9d9926c668c3f7733ddeb6fb1b8e9d1fd2
mappingCo <mapping.and.co@gmail.com>
2013-12-05 16:03:51 +0100

    edit imgDir path

--- globe/globe.js
+++ globe/globe.js
@@ -71,7 +71,7 @@ DAT.Globe = function(container, colorFn) {
 
   var overRenderer;
 
-  var imgDir = './';
+  var imgDir = '../globe/';//for highub hosting
 
   var curZoomSpeed = 0;
   var zoomSpeed = 50;
